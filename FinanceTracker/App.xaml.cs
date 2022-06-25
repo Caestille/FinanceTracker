78d43fa5452810ed34e9d4772b2ac423dc20d245
Joseph Ward <jward2100@gmail.com>
2022-06-25 01:24:36 +0100

    Add colours from a previous app and error handling for release version

--- FinanceTracker/App.xaml.cs
+++ FinanceTracker/App.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 
 namespace FinanceTracker
@@ -11,7 +9,48 @@ namespace FinanceTracker
 	/// <summary>
 	/// Interaction logic for App.xaml
 	/// </summary>
-	public partial class App : Application
+	public partial class App
 	{
+		public static string[]? StartingArgs { get; set; }
+		public static string CrashReportsDirectory => Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\FinanceTracker\CrashReports";
+
+		public App()
+		{
+			AppDomain.CurrentDomain.UnhandledException += ExceptionHandler;
+		}
+
+		private static void ExceptionHandler(object sender, UnhandledExceptionEventArgs args)
+		{
+			// Do nothing in debug to avoid spam
+#if RELEASE
+			Exception e = (Exception)args.ExceptionObject;
+			if (!Directory.Exists(CrashReportsDirectory))
+				Directory.CreateDirectory(CrashReportsDirectory);
+			string path = CrashReportsDirectory;
+
+			string exceptionText = "FinanceTracker Crash Report\n";
+
+			exceptionText += $"Date/Time: {DateTime.UtcNow} UTC\n";
+			exceptionText += $"Version: {Assembly.GetExecutingAssembly().GetName().Version}\n";
+			exceptionText += $"Source: {e.Source}\n";
+			exceptionText += $"Message: {e.Message}\n";
+			exceptionText += $"InnerException:\n{e.InnerException}";
+			exceptionText += $"Stack trace:\n{e.StackTrace}\n";
+
+			File.WriteAllText(string.Format(path + "\\FinanceTrackerCrashReport{0}.txt", DateTime.UtcNow.ToString("ddMMyyyy-HHmmss")), exceptionText);
+			MessageBox.Show(string.Format("FinancetrackerCrashReport{0}.txt written to \n" + path, DateTime.UtcNow.ToString("ddMMyyyy-HHmmss")));
+			Process.Start(path);
+#endif
+		}
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			if (e.Args.Length > 0)
+			{
+				StartingArgs = e.Args;
+			}
+
+			base.OnStartup(e);
+		}
 	}
 }
