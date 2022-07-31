150216bf9c918ca9a0ab11665152754f87dcd31f
Joseph <jward2100@gmail.com>
2022-07-31 14:37:33 +0100

    Add missing csproj changes and accounts vm

---
+++ FinanceTracker.Core/ViewModels/AccountsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CoreUtilities.Interfaces;
+using FinanceTracker.Core.Interfaces;
+
+namespace FinanceTracker.Core.ViewModels
+{
+	public class AccountsViewModel : ViewModelBase
+	{
+		public AccountsViewModel(IBankApiService bankApiService, IRegistryService registryService, string name, string defaultChildName, Guid parentBankGuid, bool holdsCreditCards) 
+			: base(name, () => new AccountViewModel(bankApiService, registryService, defaultChildName, parentBankGuid, Guid.NewGuid()))
+		{
+			var existingAccounts = registryService.GetAllSettingsInPath($@"\Banks\{parentBankGuid}\Accounts");
+			foreach (var kvp in existingAccounts)
+			{
+				var vm = new AccountViewModel(bankApiService, registryService, kvp.Value.ToString().Split('|').First(), parentBankGuid, Guid.Parse(kvp.Key));
+				vm.OriginalName = kvp.Value.ToString().Split('|').Last();
+				vm.IsCreditCard = false;
+				if (vm.IsCreditCard == holdsCreditCards)
+				{
+					AddChild(vm);
+				}
+			}
+		}
+	}
+}
