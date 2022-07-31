b443295701df2980385c94c992cdbcfcee7ac07c
Joseph <jward2100@gmail.com>
2022-07-31 14:35:15 +0100

    Move menu item templates to app.xaml so other controls can use. Add new converter

---
+++ FinanceTracker/Converters/CreditCardListFilterConverter.cs
@@ -0,0 +1,24 @@
+﻿using CoreUtilities.HelperClasses;
+using FinanceTracker.Core.ViewModels;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace FinanceTracker.Converters
+{
+	public class CreditCardListFilterConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var vms = (RangeObservableCollection<ViewModelBase>)value;
+			var allow = bool.Parse((string)parameter);
+			return vms.Where(x => (x as AccountViewModel).IsCreditCard == allow);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
