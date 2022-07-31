e04fb628b6731b9aab3c4d01eb31dc47b1b9aef3
Joseph <jward2100@gmail.com>
2022-07-31 14:35:57 +0100

    Update currency converter to always add two decimal places

--- FinanceTracker/Converters/CurrencyFormatterConverter.cs
+++ FinanceTracker/Converters/CurrencyFormatterConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
 using System.Windows.Data;
 
 namespace FinanceTracker.Converters
@@ -8,7 +9,23 @@ namespace FinanceTracker.Converters
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			return $"£ {Math.Round((double)value, 2)}";
+			var val = $"£ {Math.Round((double)value, 2)}";
+			var parts = val.Split('.');
+			if (parts.Count() == 1)
+			{
+				return val + ".00";
+			}
+			else if (parts.Count() == 2)
+			{
+				var decimals = parts.Last();
+				if (decimals.Length == 1)
+				{
+					decimals += "0";
+				}
+
+				return string.Join('.', new string[] { parts[0], decimals });
+			}
+			return string.Empty;
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
